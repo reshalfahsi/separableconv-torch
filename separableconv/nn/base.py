8897162fd209b3c4423e4a4de99ff9e9ff4ca643
reshalfahsi <resha.alfahsi@gmail.com>
2022-08-08 14:14:00 +0700

    fix indentation error

--- separableconv/nn/base.py
+++ separableconv/nn/base.py
@@ -30,8 +30,8 @@ class _SeparableConv(nn.Module):
 
     def forward(self, x):
 
-        assert self.dwconv is not None and self.pwconv is not None, "Depthwise Convolution and/or Pointwise Convolution"
-                                                                    " is/are not implemented yet."
+        assert self.dwconv is not None and self.pwconv is not None, "Depthwise Convolution and/or Pointwise Convolution is/are not implemented yet."
+
         x = self.dwconv(x)
 
         if self.dwconv_normalization is not None:
